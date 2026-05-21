5d18435b21d897f310c7e99db5a789d9c3d7dc18
Alejandro Suarez <alejandrojsuarezp@gmail.com>
2026-05-20 21:19:18 -0400

    Fix config para leer Streamlit Secrets correctamente

--- config.py
+++ config.py
@@ -2,29 +2,22 @@
 # ALVA LOGISTICS TRACKER - Configuracion Global
 # ============================================
 
-import os
 import streamlit as st
 
-def get_secret(key, default=None):
-    try:
-        return st.secrets[key]
-    except:
-        return os.environ.get(key, default)
-
 # Airtable
-AIRTABLE_TOKEN = get_secret("AIRTABLE_TOKEN")
-BASE_ID = get_secret("BASE_ID")
+AIRTABLE_TOKEN = st.secrets["AIRTABLE_TOKEN"]
+BASE_ID = st.secrets["BASE_ID"]
 
 # IDs de las tablas
-SHIPMENTS_TABLE = get_secret("SHIPMENTS_TABLE")
-PRICING_TABLE = get_secret("PRICING_TABLE")
-LOADS_TABLE = get_secret("LOADS_TABLE")
-UPDATES_LOG_TABLE = get_secret("UPDATES_LOG_TABLE")
+SHIPMENTS_TABLE = st.secrets["SHIPMENTS_TABLE"]
+PRICING_TABLE = st.secrets["PRICING_TABLE"]
+LOADS_TABLE = st.secrets["LOADS_TABLE"]
+UPDATES_LOG_TABLE = st.secrets["UPDATES_LOG_TABLE"]
 
 # Email
-EMAIL_REMITENTE = get_secret("EMAIL_REMITENTE")
-EMAIL_CONTRASENA = get_secret("EMAIL_CONTRASENA")
-EMAIL_DESTINATARIO = get_secret("EMAIL_DESTINATARIO")
+EMAIL_REMITENTE = st.secrets["EMAIL_REMITENTE"]
+EMAIL_CONTRASENA = st.secrets["EMAIL_CONTRASENA"]
+EMAIL_DESTINATARIO = st.secrets["EMAIL_DESTINATARIO"]
 
 # Limites logisticos
 DISTANCIA_MAX_MILLAS = 150
